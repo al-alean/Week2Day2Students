d8b65200bda20c590fe964e4838b8d5ae01a2d11
Albert <albert.alean@gmail.com>
2019-01-17 01:26:52 -0500

    Initial commit

---
+++ app/src/main/java/com/android/admin/week2day2_students/DatabaseConstants.java
@@ -0,0 +1,15 @@
+package com.android.admin.week2day2_students;
+
+public class DatabaseConstants {
+    public static final String DATABASE_NAME = "StudentDB";
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "student_table";
+    public static final String FIELD_NAME = "name";
+    public static final String FIELD_MAJOR = "major";
+    public static final String FIELD_MINOR = "minor";
+    public static final String FIELD_GPA = "gpa";
+    public static final String FIELD_DOB = "dob";
+    public static final String HOME_CITY = "homeCity";
+    public static final String HOME_STATE = "homeState";
+    public static final String SSN = "ssn";
+}
